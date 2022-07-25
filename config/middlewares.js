78f2b7a273ef59b39c930ebbb1bca8b9345c940d
dimasadityamukhsinin <dimasadityamukhsinin@gmail.com>
2022-07-25 20:22:54 +0700

    💄 update middleware

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,20 +1,10 @@
-module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-  {
+module.exports = {
+  settings: {
     gzip: {
       enabled: true,
       options: {
         br: false
       }
-    },
-  }
-];
+    }
+  },
+}
