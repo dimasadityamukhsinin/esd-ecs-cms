ebe21e734ea3fcfd35cfbe2b5284bea5777a0b1c
dimasadityamukhsinin <dimasadityamukhsinin@gmail.com>
2022-08-27 11:48:53 +0700

    update '

--- config/database.js
+++ config/database.js
@@ -1,15 +1,13 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: env("DATABASE_CLIENT", "postgres"),
-
+    client: 'postgres',
     connection: {
-      host: env("DATABASE_HOST", "top2.nearest.of.esd-ecs-db.internal"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "esd_ecs"),
-      user: env("DATABASE_USERNAME", "esd_ecs"),
-      password: env("DATABASE_PASSWORD", "7cmdvp2Ph5tlFAU"),
-      ssl: false,
+      host: env('DATABASE_HOST', 'localhost'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'esd_ecs_cms'),
+      user: env('DATABASE_USERNAME', 'postgres'),
+      password: env('DATABASE_PASSWORD', 'admin'),
+      ssl: env.bool('DATABASE_SSL', false),
     },
-    debug: false,
   },
 });
