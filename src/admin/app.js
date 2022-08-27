8249dcea65f7ab2e64f9be3b1c98ec36cba014a2
dimasadityamukhsinin <dimasadityamukhsinin@gmail.com>
2022-08-27 13:50:08 +0700

    updatE

--- src/admin/app.js
+++ src/admin/app.js
@@ -15,8 +15,8 @@ export default {
     translations: {
       en: {
         "app.components.LeftMenu.navbrand.title": "ESD in ECS Dashboard",
-        "app.components.LeftMenu.navbrand.workplace": "Admin panel",
-        "app.components.HomePage.welcomeBlock.content": "",
+        "app.components.LeftMenu.navbrand.workplace": "Admin Panel",
+        "app.components.HomePage.welcomeBlock.content.again": " ",
         'Auth.form.welcome.title': 'Welcome to ESD in ECS!',
         'Auth.form.welcome.subtitle': 'Log in to your ESD in ECS account',
       },
