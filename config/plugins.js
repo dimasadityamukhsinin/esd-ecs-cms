15ed3af2d91e6f6c35f184173f1de033d3a556e2
dimasadityamukhsinin <dimasadityamukhsinin@gmail.com>
2022-11-20 18:18:28 +0700

    update

--- config/plugins.js
+++ config/plugins.js
@@ -18,4 +18,22 @@ module.exports = ({ env }) => ({
   'import-export-entries': {
     enabled: true,
   },
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST', 'smtp.gmail.com'),
+        port: env('SMTP_PORT', 465),
+        auth: {
+          user: env('GMAIL_USER'),
+          pass: env('GMAIL_PASSWORD'),
+        },
+        // ... any custom nodemailer options
+      },
+      settings: {
+        defaultFrom: 'admin@esd-in-ecs.com',
+        defaultReplyTo: 'admin@esd-in-ecs.com',
+      },
+    },
+  },
 })
